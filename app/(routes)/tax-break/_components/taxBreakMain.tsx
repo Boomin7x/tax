49ccca88674b4e380a8ac57d75244939a59fa0be
Kodiah Bertrand <kodiahbetro@gmail.com>
2025-01-23 09:56:48 +0100

    tax break delete

--- app/(routes)/tax-break/_components/taxBreakMain.tsx
+++ app/(routes)/tax-break/_components/taxBreakMain.tsx
@@ -1,16 +1,18 @@
 "use client";
 import useStore from "@/app/store/useStore";
-import React, { useEffect, useState } from "react";
-import CreatetaxBreakModal, {
-  ICreatetaxBreakModal,
-} from "./createtaxBreakModal";
-import { Button } from "@/components/ui/button";
-import useGetAllTaxBreak from "../_hooks/useGetAllTaxBreak";
-import { ITaxBreak } from "../_utils/type";
-import { taxBreakColumn } from "../_utils/column";
 import { DataTable } from "@/components/dataTable";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { IDeleteModalState, ISheet, ISheetState } from "../../types";
+import useGetAllTaxBreak from "../_hooks/useGetAllTaxBreak";
+import { taxBreakColumn } from "../_utils/column";
+import { ITaxBreak } from "../_utils/type";
+import CreatetaxBreakModal from "./createtaxBreakModal";
 import TaxBreakDetailsSheet from "./taxBreakDetailsSheet";
-import { ISheet, ISheetState } from "../../types";
+import DeleteDailog, { IDeleteDailog } from "@/components/deleteDailog";
+import useMessage from "@/hooks/useMessage";
+import useDeleteTaxBreak from "../_hooks/useDeleteTaxBreak";
+import { isAxiosError } from "axios";
 
 const TaxBreakMain = () => {
   const { handleTitle } = useStore();
@@ -18,8 +20,27 @@ const TaxBreakMain = () => {
     handleTitle("Tax break");
   }, []);
 
+  const message = useMessage();
+  const { mutate, isPending } = useDeleteTaxBreak();
+  const { data } = useGetAllTaxBreak({ page: 1, limit: 10 });
+  const taxBreakData = data?.data as ITaxBreak[];
+
+  const [deleteModal, setDeleteModal] = useState<IDeleteModalState>();
   const [createModal, setCreateModal] = useState<ISheetState<ITaxBreak>>();
   const [detailsModal, setDetailsModal] = useState<ISheetState<ITaxBreak>>();
+
+  const deleteFn = (id: string) => {
+    mutate(id, {
+      onSuccess: () => {
+        message({ message: "Tax break deleted", status: "success" });
+        setDeleteModal(undefined);
+      },
+      onError: (error) => {
+        if (isAxiosError(error))
+          message({ message: error?.response?.data?.message, status: "error" });
+      },
+    });
+  };
 
   const createtaxBreakModalProps: ISheet<ITaxBreak> = {
     data: createModal?.data,
@@ -33,9 +54,13 @@ const TaxBreakMain = () => {
     onClose: () => setDetailsModal(undefined),
   };
 
-  const { data } = useGetAllTaxBreak({ page: 1, limit: 10 });
-  const taxBreakData = data?.data as ITaxBreak[];
-  console.log({ data });
+  const deleteModalProps: IDeleteDailog = {
+    action: deleteFn,
+    desc: "This will delete the Tax break and this process is irreversible",
+    isLoading: isPending,
+    onClose: () => setDeleteModal(undefined),
+    open: deleteModal as IDeleteModalState,
+  };
 
   return (
     <div className="flex flex-col">
@@ -44,7 +69,7 @@ const TaxBreakMain = () => {
       </Button>
       <DataTable
         columns={taxBreakColumn({
-          onDelete: () => {},
+          onDelete: (data) => setDeleteModal({ id: data?.uuid, open: true }),
           onDetails: (data) => setDetailsModal({ isopen: true, data }),
           onEdit: (data) => setCreateModal({ isopen: true, data }),
         })}
@@ -56,6 +81,7 @@ const TaxBreakMain = () => {
       {detailsModal?.isopen ? (
         <TaxBreakDetailsSheet {...taxBreakDetailsProps} />
       ) : null}
+      {deleteModal?.open ? <DeleteDailog {...deleteModalProps} /> : null}
     </div>
   );
 };
