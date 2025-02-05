defd1203f3ff3047addeaf845e80ad67cfcaef04
Kodiah Bertrand <kodiahbetro@gmail.com>
2025-02-05 15:24:11 +0100

    shaed saved

---
+++ context/MuiProvider.tsx
@@ -0,0 +1,15 @@
+// _app.tsx
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import type { AppProps } from "next/app";
+
+const theme = createTheme();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
